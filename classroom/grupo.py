7b211243c6f591af4abd35a2a1322c99566efbff
Daimon-22 <elvelandiar@unal.edu.co>
2023-03-06 20:57:54 -0500

    inte

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -8,7 +8,7 @@ class Grupo:
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
@@ -21,9 +21,6 @@ class Grupo:
     def __str__(self):
         return f"Grupo de estudiantes: {self._grupo}"
 
-    @ classmethod
-    def asignarNombre(cls, nombre="Grado 10"):
-        cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
